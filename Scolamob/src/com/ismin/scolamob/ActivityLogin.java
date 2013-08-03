c04073c77bbc026c87b209aa03fa275f51bed86c
Gaëtan MacBook <gaetanmaisse@192.168.127.45>
2013-08-03 19:03:04 +0200

    Test Commit

--- Scolamob/src/com/ismin/scolamob/ActivityLogin.java
+++ Scolamob/src/com/ismin/scolamob/ActivityLogin.java
@@ -13,11 +13,6 @@ public class ActivityLogin extends Activity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_login, menu);
-        return true;
-    }
+ 
     
 }
